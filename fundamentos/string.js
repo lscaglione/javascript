2dc6f52ff088281654a2ebdaa647910a4fed5f55
lscaglione <luccascaglione1@gmail.com>
2021-03-15 19:06:07 -0300

    15/03

---
+++ fundamentos/string.js
@@ -0,0 +1,9 @@
+const escola = 'cod3r'
+
+console.log(escola.charAt(4)) //charAt mostra a posição do caractere
+console.log(escola.charAt(5)) 
+console.log(escola.charCodeAt(3)) //mostra o numero da tabela unicode
+console.log(escola.indexOf(3)) //mostra o indice da string
+
+console.log(escola.substring(1)) //mostra a string sem o índice escolhido
+console.log(escola.substring(0, 3)) //mostra a palabra do indice 0 por exemplo até antes do 3
